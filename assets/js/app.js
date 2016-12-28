07f96211f4c1f6b5d283ff342b0f3d0a74e8fa34
rikumax25 <jorgev259@outlook.es>
2016-12-27 21:57:16 -0600

    Added pokemon list (and proper selection detection)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,17 @@
 var pokemon_url = new Array();
 var count = 0;
+var count_id = 0;
+var debug;
 
 $(document).ready(function(){
-        poke_start("http://pokeapi.co/api/v2/pokemon");
+    poke_start("http://pokeapi.co/api/v2/pokemon");
+    
+    $('#poke_input').on('input',function() {
+        var opt = $('option[value="'+$(this).val()+'"]');
+        debug= opt;
+        console.log(opt[0].dataset.id);
+    });
+
 });
 
 function poke_start(url){    
@@ -14,19 +23,26 @@ function poke_start(url){
             var results = result.results;
              results.forEach(function(result){
                  pokemon_url[count] = result;                 
-                 execute_async("add_to_list",result.name);
-                 
+                 execute_async("add_to_list",result);                 
                  count++;
              });
-            console.log(pokemon_url);
         }else{
+            $("#cover_up").hide();
             alert("Finished loading pokemon list");
         }
-        });
+        }).fail(function(){
+        $("#cover_up").hide();
+        
+    });
 }
 
-function add_to_list(name){
-       $("#pokemon_list").append("<option value='" + name + "'>"); 
+function pokemon_change(){
+    
+}
+
+function add_to_list(result){
+       $("#pokemon_list").append("<option data-id='" + count_id + "' value='" + result.name + "'>"); 
+        count_id++;
 }
 
 function execute_async(func_name,args){
